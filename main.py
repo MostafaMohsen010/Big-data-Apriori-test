01f5edc637713f0233aa5512c2080d8bb85f8b3a
AhmedSamir <ahmed299777@gmail.com>
2020-05-13 18:33:01 +0200

    modified main

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-#from loading_data import df,dic,transactions
 from generate_rules import get_rules_eff,compute_rules
 from generate_item_sets import compute_item_set1
 import argparse
@@ -7,11 +6,11 @@ parser = argparse.ArgumentParser()
 
 parser.add_argument('--algorithm', type=int,default=0)#0 for efficient_apriori, 1 for our algorithm
 parser.add_argument('--min_support', type=float,default=.1)
-parser.add_argument('--min_confiedence',type=float,default=1)
+parser.add_argument('--min_confidence',type=float,default=1)
 args=parser.parse_args()
 
 if(args.algorithm == 0):
-	get_rules_eff(args.min_support,args.min_confiedence)
+	get_rules_eff(args.min_support,args.min_confidence)
 else:
-	compute_rules(args.min_support,args.min_confiedence)
+	compute_rules(args.min_support,args.min_confidence)
 
