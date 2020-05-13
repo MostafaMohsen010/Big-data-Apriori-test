d02c84ef290e73a2e0ce0a2520493148bbb3ac2b
AhmedSamir <ahmed299777@gmail.com>
2020-05-13 18:32:38 +0200

    modified generate_rules

--- generate_rules.py
+++ generate_rules.py
@@ -2,41 +2,44 @@ from efficient_apriori import apriori
 import enum
 from loading_data import df,dic,transactions,discription
 from generate_item_sets import compute_item_set1,trans_count
-
-
+import time
 sub_sets=[]
 rules=[]
 
 def get_rules_eff(min_sup,min_confid):
-	itemset,rules = apriori(transactions, min_support=min_sup, min_confidence=min_confid)
-	print("number of rules = ",len(rules),"\n\n")
-	for r in rules:
-	    print("(",end="")    
-	    for c,l in enumerate(r.lhs):
-	        classs=dic[l]
-	        sub_class=classs[1]
-	        if df.columns[classs[0]] in discription:
-	        	sub_class=discription[df.columns[classs[0]]][sub_class]
+  t1=time.time()
+  itemset,rules=apriori(transactions, min_support=min_sup, min_confidence=min_confid)
+  t2=time.time()
+  print("Time Cost = ",'%.3f'%(t2-t1),"seconds")
+  print("number of rules = ",len(rules),"\n\n")
+  
+  for r in rules:
+    print("(",end="")    
+    for c,l in enumerate(r.lhs):
+      classs=dic[l]
+      sub_class=classs[1]
+      if df.columns[classs[0]] in discription:
+      	sub_class=discription[df.columns[classs[0]]][sub_class]
 
-	        print(df.columns[classs[0]],"=",sub_class,end="")
-	        if(c == len(r.lhs)-1):
-	            break
-	        print(" , ",end="")
+      print(df.columns[classs[0]],"=",sub_class,end="")
+      if(c == len(r.lhs)-1):
+        break
+      print(" , ",end="")
+    print(")"," -> ","(",end=" ")
 
-	    print(")"," -> ","(",end=" ")
+    for c,l in enumerate(r.rhs):
+      classs=dic[l]
+      sub_class=classs[1]
+      if df.columns[classs[0]] in discription:
+        sub_class=discription[df.columns[classs[0]]][sub_class]
 
-	    for c,l in enumerate(r.rhs):
-	        classs=dic[l]
-	        sub_class=classs[1]
-	        if df.columns[classs[0]] in discription:
-	        	sub_class=discription[df.columns[classs[0]]][sub_class]
-
-	        print(df.columns[classs[0]],"=",sub_class,end="")
-	        if(c == len(r.rhs)-1):
-	            break
-	        print(" , ",end="")
-	    print(") Confidence=",r.confidence,"  Lift=",r.lift,"\n\n")
-
+      print(df.columns[classs[0]],"=",sub_class,end="")
+      if(c == len(r.rhs)-1):
+        break
+      print(" , ",end="")
+    
+    print(") Confidence=",'%.3f'%r.confidence,"  Lift=",'%.3f'%r.lift,"\n\n")
+    
 
 class rule():
   def __init__(self,lhs,rhs,confid,sup_lhs,sup_rhs,sup_lhsandrhs):
@@ -48,7 +51,6 @@ class rule():
     self.sup_lhsandrhs=sup_lhsandrhs
   
   def visualize(self): 
-    
     print("(",end="")       
     for c,l in enumerate(self.lhs):
       classs=dic[l][0]
@@ -71,7 +73,7 @@ class rule():
       if(c == len(self.rhs)-1):
         break
       print(" & ",end="")
-    print(") [ Confidence=",self.confid," , Lift=",self.sup_lhsandrhs/(self.sup_lhs*self.sup_rhs)," , Leverage=",self.sup_lhsandrhs-(self.sup_lhs*self.sup_rhs),"] \n\n")
+    print(") [ Confidence=",'%.3f'%self.confid," , Lift=",'%.3f'%(self.sup_lhsandrhs/(self.sup_lhs*self.sup_rhs))," , Leverage=",'%.3f'%(self.sup_lhsandrhs-(self.sup_lhs*self.sup_rhs)),"] \n\n")
 
 
 def support2(trans1):
@@ -102,6 +104,7 @@ def get_rules(item_set,min_conf):
       rules.append(r)
 
 def compute_rules(min_sup,min_conf):
+  t1=time.time()
   it =compute_item_set1(min_sup)
   global rules
   rules=[]
@@ -111,6 +114,8 @@ def compute_rules(min_sup,min_conf):
         sub_sets=[]      
         find_all_subsets(it[i][j].copy(),[0],0)
         get_rules(it[i][j],min_conf)
+  t2=time.time()
+  print("Time Cost =",'%.2f'%(t2-t1),"seconds")
   print("num_of_rules",len(rules))
   for r in rules:
   	r.visualize()
